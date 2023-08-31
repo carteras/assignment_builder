c95263bbeb81b316c3fd2b0c5a099d3d851bf7a9
Adam Carter <adam.carter@ed.act.edu.au>
2023-08-31 15:39:42 +1000

    added assignment sheet

--- word/assignment_maker_docx.py
+++ word/assignment_maker_docx.py
@@ -55,7 +55,7 @@ def config_subject(config_location: str) -> Subject:
     tertiary = config.get("ACCREDITATION", 'Tertiary')
     assessments = {}
     assessments['AI1'] = config.get("ASSESSMENT_ITEMS", 'AI1')
-    # assessments['AI2'] = config.get("ASSESSMENT_ITEMS", 'AI2')
+    assessments['AI2'] = config.get("ASSESSMENT_ITEMS", 'AI2')
     # assessments['AI3'] = config.get("ASSESSMENT_ITEMS", 'AI3')
     # assessments['AI4'] = config.get("ASSESSMENT_ITEMS", 'AI4')
     years = []
